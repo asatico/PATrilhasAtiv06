713a6a57389c034426c1867109c5ab73950f36a7
Guilherme50 <guilhermex12g@gmail.com>
2025-02-06 11:07:55 -0300

    Create F.cs

---
+++ F.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+    	
+    	Console.Write("Escreva um número primo:");
+    	int quant = int.Parse(Console.ReadLine());
+    	Console.Clear();
+    	
+    	int numMaior = int.MinValue;
+    	
+    	
+    	
+    	for (int i = 1; i <= quant; i++) {
+    		Console.WriteLine("Digite o {0}º número ",i);
+    		int num = int.Parse(Console.ReadLine());
+    		
+    		
+    		if (num > numMaior) {
+    			numMaior = num;
+    		} 
+    		
+    		
+    	}
+    	
+    	Console.WriteLine("O  maior  número  inserido  foi: " + numMaior);
+    	
+		
+    Console.WriteLine("Presione algo para sair . . . ");
+	Console.ReadKey(true);
+    }
+}
