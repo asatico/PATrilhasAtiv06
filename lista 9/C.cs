11721586a10df6964e3b7040171d968246a77ac6
Guilherme50 <guilhermex12g@gmail.com>
2025-02-07 14:05:58 -0300

    Update C.cs

--- lista 9/C.cs
+++ lista 9/C.cs
@@ -4,16 +4,15 @@ class Program
 {
     static void Main()
     {
-		double[] numeros = {1.5, 2.5,3.5,4.5,5.5};
+	double[] numeros = {1.5, 2.5,3.5,4.5,5.5};
         double soma = 0;
-
-        foreach (double num in numeros){
-            soma += num;
+        for (int i = 0; i < numeros.Length; i++){
+            soma += numeros[i];
         }
         double meidia = soma / numeros.Length;
         Console.WriteLine("A media dos números do vertor é:\n"+meidia);
       
       Console.WriteLine("Presione algo para sair . . . ");
-	  Console.ReadKey(true);
+      Console.ReadKey(true);
     }
 }
